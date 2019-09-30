7320a240126f0d2cc5baf4eb9a052e6f1fbded57
airfix97 <anky199714@gmail.com>
2019-09-30 22:32:00 +0530

    Update rx.ino

--- rx.ino
+++ rx.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 void setup() {
@@ -10,15 +11,16 @@ void setup() {
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
+
 void loop() {
   if (radio.available()) {
     float text;
     float text1;
-    radio.read(&text, sizeof(text));
-    radio.read(&text1, sizeof(text1));
+    radio.read(&text, sizeof(text));             // reads the first incoming data i:e temperature
+    radio.read(&text1, sizeof(text1));            // read the second incoming data i:e humidity
   
-    Serial.print(text);
-    Serial.print("-------");
-    Serial.println(text1);
+    Serial.print(text);                           //prints temperature on serial monitor
+    Serial.print("-------");                  
+    Serial.println(text1);                        // prints humidity on serial monitor
   }
 }
